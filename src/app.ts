8769b47a5e425e15399789510db84605fd4081f0
Evert Pot <me@evertpot.com>
2020-03-03 11:54:34 -0500

    Adding a few more useful default middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 import { Application } from '@curveball/core';
+import accessLog from '@curveball/accesslog';
+import problem from '@curveball/problem';
+import bodyParser from '@curveball/bodyparser';
+
+console.log('⚾ Curveball v%s', require('@curveball/core/package.json').version);
 
 const app = new Application();
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8500;
+// The accesslog middleware shows all requests and responses on the cli.
+app.use(accessLog());
+
+// The problem middleware turns exceptions into application/problem+json error
+// responses.
+app.use(problem());
+
+// The bodyparser middleware is responsible for parsing JSON and url-encoded
+// request bodies, and populate ctx.request.body.
+app.use(bodyParser());
+
+// The HTTP port can be overridden via the 'PORT' environment variable.
+const port = process.env.PORT ? parseInt(process.env.PORT, 12) : 8500;
 app.listen(8500);
 
 console.log('Listening on port %i', port);
